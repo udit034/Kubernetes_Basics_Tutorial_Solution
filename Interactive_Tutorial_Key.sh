7cfde586840b8cd66db04fbdb22fc0d87aad9923
udit034 <udit.mehata@gmail.com>
2018-06-24 14:01:23 -0400

    Tutorial 1, 2

--- Interactive_Tutorial_Key.sh
+++ Interactive_Tutorial_Key.sh
@@ -4,3 +4,10 @@ kubectl version
 kubectl cluster-info
 kubectl get nodes
 
+kubectl run kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080
+kubectl get deployments
+kubectl proxy
+curl http://localhost:8001/version
+export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo Name of the Pod: $POD_NAME
+curl http://localhost:8001/api/v1/namespaces/default/pods/$POD_NAME/proxy/
