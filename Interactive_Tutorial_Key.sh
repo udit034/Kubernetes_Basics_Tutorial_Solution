cd3f6d0d934fc91440d136ed4a542a07135478ae
udit034 <udit.mehata@gmail.com>
2018-06-25 00:44:43 -0400

    Tutorial 5

--- Interactive_Tutorial_Key.sh
+++ Interactive_Tutorial_Key.sh
@@ -43,3 +43,16 @@ kubectl delete service -l run=kubernetes-bootcamp
 kubectl get services
 curl $(minikube ip):$NODE_PORT
 kubectl exec -ti $POD_NAME curl localhost:8080
+
+kubectl get deployments
+kubectl scale deployments/kubernetes-bootcamp --replicas=4
+kubectl get deployments
+kubectl get pods -o wide
+kubectl describe deployments/kubernetes-bootcamp
+kubectl describe services/kubernetes-bootcamp
+export NODE_PORT=$(kubectl get services/kubernetes-bootcamp -o go-template='{{(index .spec.ports 0).nodePort}}')
+echo NODE_PORT=$NODE_PORT
+curl $(minikube ip):$NODE_PORT
+kubectl scale deployments/kubernetes-bootcamp --replicas=2
+kubectl get deployments
+kubectl get pods -o wide
