972932941cb790d5ee4bbe6b73e5f77a17da172c
udit034 <udit.mehata@gmail.com>
2018-06-25 01:06:59 -0400

    Tutorial Complete

--- Interactive_Tutorial_Key.sh
+++ Interactive_Tutorial_Key.sh
@@ -56,3 +56,21 @@ curl $(minikube ip):$NODE_PORT
 kubectl scale deployments/kubernetes-bootcamp --replicas=2
 kubectl get deployments
 kubectl get pods -o wide
+
+kubectl get deployments
+kubectl get pods
+kubectl describe pods
+kubectl set image deployments/kubernetes-bootcamp kubernetes-bootcamp=jocatalin/kubernetes-bootcamp:v2
+kubectl describe services/kubernetes-bootcamp
+export NODE_PORT=$(kubectl get services/kubernetes-bootcamp -o go-template='{{(index .spec.ports 0).nodePort}}')
+echo NODE_PORT=$NODE_PORT
+curl $(minikube ip):$NODE_PORT
+kubectl rollout status deployments/kubernetes-bootcamp
+kubectl describe pods
+kubectl set image deployments/kubernetes-bootcamp kubernetes-bootcamp=gcr.io/google-samples/kubernetes-bootcamp:v10
+kubectl get deployments
+kubectl get pods
+kubectl describe pods
+kubectl rollout undo deployments/kubernetes-bootcamp
+kubectl get pods
+kubectl describe pods
