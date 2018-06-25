140cadabfcfa8e3d46bb7ea45b2f78352a5751d4
udit034 <udit.mehata@gmail.com>
2018-06-24 22:34:09 -0400

    Tutorial 4

--- Interactive_Tutorial_Key.sh
+++ Interactive_Tutorial_Key.sh
@@ -23,3 +23,23 @@ kubectl exec $POD_NAME env
 kubectl exec -ti $POD_NAME bash
 cat server.js
 exit
+
+kubectl get pods
+kubectl get services
+kubectl expose deployment/kubernetes-bootcamp --type="NodePort" --port 8080
+kubectl get services
+export NODE_PORT=$(kubectl get services/kubernetes-bootcamp -o go-template='{{(index .spec.ports 0).nodePort}}')
+echo NODE_PORT=$NODE_PORT
+curl $(minikube ip):$NODE_PORT
+kubectl describe deployment
+kubectl get pods -l run=kubernetes-bootcamp
+kubectl get services -l run=kubernetes-bootcamp
+export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo Name of the Pod: $POD_NAME
+kubectl label pod $POD_NAME app=v1
+kubectl describe pods $POD_NAME
+kubectl get pods -l app=v1
+kubectl delete service -l run=kubernetes-bootcamp
+kubectl get services
+curl $(minikube ip):$NODE_PORT
+kubectl exec -ti $POD_NAME curl localhost:8080
